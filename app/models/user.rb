640665f507bf82cfb94d413bc307b3ef8d50ba4f
Maria Elvira <medevuono@gmail.com>
2018-03-29 12:04:57 -0500

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true, format: /@/
+  validates :password, presence: true, on: :create
+  validates :password, length: { in: 6..20 }, allow_nil: true
+end
